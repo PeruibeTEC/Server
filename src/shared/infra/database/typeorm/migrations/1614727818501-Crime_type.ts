f8831180995ff2a91b661b6c6ae18a5e5e386225
Leticia Silverio <leticia.silverio09@gmail.com>
2021-03-03 13:14:32 -0300

    feat(database): added a new migrations about crime

--- src/shared/infra/database/typeorm/migrations/1614727818501-Crime_type.ts
+++ src/shared/infra/database/typeorm/migrations/1614727818501-Crime_type.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CrimeLocation1614655492295 implements MigrationInterface {
+export class CrimeType1614727818501 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'tb_crime_location',
+        name: 'tb_crime_type',
         columns: [
           {
             name: 'id',
@@ -14,32 +14,10 @@ export class CrimeLocation1614655492295 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'street',
+            name: 'name',
             type: 'varchar',
-            length: '150',
-          },
-          {
-            name: 'number',
-            type: 'varchar',
-            length: '10',
+            length: '45',
             isNullable: true,
-          },
-          {
-            name: 'district',
-            type: 'varchar',
-            length: '100',
-          },
-          {
-            name: 'latitude',
-            type: 'decimal',
-            precision: 10,
-            scale: 7,
-          },
-          {
-            name: 'longitude',
-            type: 'decimal',
-            precision: 10,
-            scale: 7,
           },
           {
             name: 'created_at',
@@ -57,6 +35,6 @@ export class CrimeLocation1614655492295 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('tb_crime_location');
+    await queryRunner.dropTable('tb_crime_type');
   }
 }

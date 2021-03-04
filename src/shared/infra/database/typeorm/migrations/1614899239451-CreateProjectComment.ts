ba210f621fe8416cdf574e4936251020273a2e2c
PocketLocked <bertoluzzi2004@gmail.com>
2021-03-04 20:43:26 -0300

    feat(database): create ProjectComment migration

---
+++ src/shared/infra/database/typeorm/migrations/1614899239451-CreateProjectComment.ts
@@ -0,0 +1,69 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateProjectComment1614899239451 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tb_project_comment',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'content',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'public_project_id',
+            type: 'uuid',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'tb_project_comment',
+      new TableForeignKey({
+        name: 'projectId',
+        columnNames: ['public_project_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'tb_public_project',
+        onDelete: 'SET NULL',
+        onUpdate: 'RESTRICT',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'tb_project_comment',
+      new TableForeignKey({
+        name: 'userId',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'tb_user',
+        onDelete: 'SET NULL',
+        onUpdate: 'RESTRICT',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('tb_project_comment', 'projectId');
+    await queryRunner.dropForeignKey('tb_project_comment', 'userId');
+    await queryRunner.dropTable('tb_project_comment');
+  }
+}

75c5f4eb068c5ba373bb53ead70553294b05404c
hmartiins <hmartins224@gmail.com>
2021-03-07 14:58:08 -0300

    feat(entities): create user entity

---
+++ src/modules/user/infra/typeorm/entities/User.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('tb_user')
+export default class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 45,
+  })
+  name: string;
+
+  @Column({
+    length: 45,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    length: 160,
+    nullable: true,
+  })
+  small_biography: string;
+
+  @Column({
+    nullable: true,
+  })
+  photo: string;
+
+  @Column()
+  is_tourist: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}

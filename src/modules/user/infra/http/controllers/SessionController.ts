407cded917e2317185c9bf7fe47d2fadfb7d93fe
PocketLocked <bertoluzzi2004@gmail.com>
2021-03-19 18:19:28 -0300

    feat: create session controller and routes

---
+++ src/modules/user/infra/http/controllers/SessionController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import AuthenticateUserService from '@modules/user/services/UserAuthenticationService';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const authenticateUser = container.resolve(AuthenticateUserService);
+
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    // @ts-expect-error ⠀⠀⠀
+    delete user.password;
+
+    return response.json({
+      user,
+      token,
+    });
+  }
+}

59047e3cf28daf5d42a41e475d69a8806154fea0
PocketLocked <bertoluzzi2004@gmail.com>
2021-03-20 16:25:32 -0300

    feat(user): create forgot and reset password features

--- src/shared/infra/http/config/routes.ts
+++ src/shared/infra/http/config/routes.ts
@@ -2,11 +2,13 @@ import { Express, Router } from 'express';
 
 import usersRouter from '@modules/user/infra/http/routes/user.routes';
 import sessionsRouter from '@modules/user/infra/http/routes/session.routes';
+import passwordRouter from '@modules/user/infra/http/routes/password.routes';
 
 export default (app: Express): void => {
   const router = Router();
   router.use('/user', usersRouter);
   router.use('/session', sessionsRouter);
+  router.use('/password', passwordRouter);
 
   app.use('/api', router);
 };

87d67a5c94e6c2ef2be25c5d07266bbace33e0b7
Diogo Braz <diogo.braz01@etec.sp.gov.br>
2021-03-20 13:23:28 -0300

    feat(user): create user update feature

--- src/modules/user/infra/http/routes/user.routes.ts
+++ src/modules/user/infra/http/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import UsersController from '../controllers/UserController';
+import ensureAuthenticate from '../middlewares/ensureAuthenticate';
 
 const usersRouter = Router();
 const usersController = new UsersController();
@@ -8,5 +9,6 @@ const usersController = new UsersController();
 usersRouter.post('/', usersController.create);
 usersRouter.delete('/:id', usersController.delete);
 usersRouter.get('/:id', usersController.show);
+usersRouter.put('/', usersController.update, ensureAuthenticate);
 
 export default usersRouter;

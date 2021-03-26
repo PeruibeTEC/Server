34c0f2bb125fbad577612a589df6ea8b776f2a57
PocketLocked <bertoluzzi2004@gmail.com>
2021-03-26 17:27:11 -0300

    feat(migration): create CreateTHeftTable migration

---
+++ src/shared/infra/database/typeorm/migrations/1616788682710-CreateTheftTable.ts
@@ -0,0 +1,71 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateTheftTable1616788682710 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tb_theft',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'date',
+            type: 'date',
+          },
+          {
+            name: 'time',
+            type: 'time',
+            isNullable: true,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            length: '200',
+            isNullable: true,
+          },
+          {
+            name: 'theft_location_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'tb_theft',
+      new TableForeignKey({
+        name: 'TheftLocationId',
+        columnNames: ['theft_location_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'tb_theft_location',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('tb_theft', 'TheftLocationId');
+    await queryRunner.dropTable('tb_theft');
+  }
+}

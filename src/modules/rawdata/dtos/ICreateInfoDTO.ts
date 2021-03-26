44a5e9a1d4d93e546a8d06812e536a2e03e51756
hmartiins <hmartins224@gmail.com>
2021-03-26 16:06:08 -0300

    feat(rawdata): create show feature

--- src/modules/rawdata/dtos/ICreateInfoDTO.ts
+++ src/modules/rawdata/dtos/ICreateInfoDTO.ts
@@ -1,3 +1,8 @@
+import InfoAddress from '../infra/typeorm/entities/InfoAddress';
+import InfoType from '../infra/typeorm/entities/InfoType';
+
 export default interface ICreateInfoDTO {
   telephone: string;
+  info_address_id: InfoAddress;
+  info_type_id: InfoType;
 }
--- src/modules/rawdata/infra/http/controllers/InfoController.ts
+++ src/modules/rawdata/infra/http/controllers/InfoController.ts
@@ -4,14 +4,19 @@ import { container } from 'tsyringe';
 
 import CreateInfoService from '@modules/rawdata/services/info/CreateInfoService';
 import DeleteInfoService from '@modules/rawdata/services/info/DeleteInfoService';
+import ShowInfoService from '@modules/rawdata/services/info/ShowInfoService';
 
 export default class InfoController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { telephone } = request.body;
+    const { telephone, info_address_id, info_type_id } = request.body;
 
     const createInfo = container.resolve(CreateInfoService);
 
-    const info = await createInfo.execute({ telephone });
+    const info = await createInfo.execute({
+      telephone,
+      info_address_id,
+      info_type_id,
+    });
 
     return response.json(info);
   }
@@ -26,5 +31,15 @@ export default class InfoController {
     return response
       .status(200)
       .json({ message: `Number ${telephone} deleted ` });
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const info_id: string = (request.params as unknown) as string;
+
+    const showInfo = container.resolve(ShowInfoService);
+
+    const info = await showInfo.execute({ info_id });
+
+    return response.status(200).json({ info });
   }
 }

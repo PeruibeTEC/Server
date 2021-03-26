7f342ce080ecc2003585e2d1e6653d7e313aa539
PocketLocked <bertoluzzi2004@gmail.com>
2021-03-26 17:27:50 -0300

    feat(migration): create CreateTouristSpotTable migration

---
+++ src/shared/infra/database/typeorm/migrations/1616788879533-CreateTouristSpotTable.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTouristSpotTable1616788879533 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tb_tourist_spot',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            length: '3000',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            precision: 10,
+            scale: 7,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            precision: 10,
+            scale: 7,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tb_tourist_spot');
+  }
+}

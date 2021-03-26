d1ded36ad5bdbfb168c06f6afd73a60d997f3dbc
Diogo Braz <grisha@pop-os.localdomain>
2021-03-25 23:47:04 -0300

    feat(rawdata): create delete info controller

--- src/modules/rawdata/infra/http/controllers/InfoController.ts
+++ src/modules/rawdata/infra/http/controllers/InfoController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateInfoService from '@modules/rawdata/services/CreateInfoService';
+import DeleteInfoService from '@modules/rawdata/services/DeleteInfoService';
 
 export default class InfoController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -13,5 +14,15 @@ export default class InfoController {
     const info = await createInfo.execute({ telephone });
 
     return response.json(info);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { telephone } = request.body;
+
+    const deleteInfo = container.resolve(DeleteInfoService);
+
+    await deleteInfo.execute({ telephone });
+
+    return response.status(200).json({ message: `Number ${telephone} deleted `})
   }
 }

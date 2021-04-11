4607a8c59cec60003c201b3419a13e7ace5a4135
hmartiins <hmartins224@gmail.com>
2021-04-11 12:50:37 -0300

    feat(rawdata): create update feature in TouristSpot service

--- src/modules/rawdata/infra/http/routes/touristspot.routes.ts
+++ src/modules/rawdata/infra/http/routes/touristspot.routes.ts
@@ -9,6 +9,6 @@ touristSpotRouter.post('/', touristSpotController.create);
 touristSpotRouter.get('/:id', touristSpotController.show);
 touristSpotRouter.delete('/', touristSpotController.delete);
 touristSpotRouter.get('/', touristSpotController.index);
-// touristSpotRouter.put('/', touristSpotController.update);
+touristSpotRouter.put('/', touristSpotController.update);
 
 export default touristSpotRouter;

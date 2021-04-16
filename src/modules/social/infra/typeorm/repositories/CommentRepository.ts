37bbe0ea8633f574a733279a1c613879a1aa8c39
hmartiins <hmartins224@gmail.com>
2021-04-15 21:19:52 -0300

    feat(social): create TypeOrm Repositories for Social module

---
+++ src/modules/social/infra/typeorm/repositories/CommentRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from 'typeorm';
+
+import ICommentRepository from '@modules/social/repositories/ICommentRepository';
+import ICommentDTO from '@modules/social/dtos/ICommentDTO';
+
+import Comment from '../entities/Comment';
+
+export default class CommentRepository implements ICommentRepository {
+  private ormRepository: Repository<Comment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Comment);
+  }
+
+  public async findAllByPost(): Promise<Comment[]> {
+    const comentsPost = await this.ormRepository.find();
+
+    return comentsPost;
+  }
+
+  public async create(infoData: ICommentDTO): Promise<Comment> {
+    const comment = this.ormRepository.create(infoData);
+
+    await this.ormRepository.save(comment);
+
+    return comment;
+  }
+
+  public async delete(id: string): Promise<string> {
+    await this.ormRepository.delete(id);
+
+    return `Comment_id: ${id} deleted`;
+  }
+
+  public async save(commentPost: Comment): Promise<Comment> {
+    return this.ormRepository.save(commentPost);
+  }
+}

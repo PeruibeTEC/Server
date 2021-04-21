a8545d6f581f68297918fb854b6ce21d0af77c55
João Victor da Silva <jv782063@gmail.com>
2021-04-20 21:29:00 -0300

    feat(eventuser): implements date checking on creation and update

--- src/modules/event/services/eventUser/CreateEventUserService.ts
+++ src/modules/event/services/eventUser/CreateEventUserService.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/infra/http/errors/AppError';
 import EventUser from '@modules/event/infra/typeorm/entities/EventUser';
 import IEventUserDTO from '@modules/event/dtos/IEventUserDTO';
+import IDatefnsProvider from '@shared/providers/DatefnsProvider/models/IDatefnsProvider';
 import IEventUserRepository from '../../repositories/IEventUserRepository';
 
 @injectable()
@@ -8,6 +10,9 @@ export default class CreateEventUserService {
   constructor(
     @inject('EventUserRepository')
     private eventUserRepository: IEventUserRepository,
+
+    @inject('DatefnsProvider')
+    private dateFnsProvider: IDatefnsProvider,
   ) {}
 
   public async execute({
@@ -19,6 +24,15 @@ export default class CreateEventUserService {
     event_type_id,
     user_id,
   }: IEventUserDTO): Promise<EventUser> {
+    const stringDate = String(date);
+    const thisDateisAfter = await this.dateFnsProvider.thisDateIsAfter(
+      stringDate,
+    );
+
+    if (!thisDateisAfter) {
+      throw new AppError('The date provided is not valid', 400);
+    }
+
     const eventUser = await this.eventUserRepository.create({
       name,
       date,
--- src/modules/event/services/eventUser/UpdateEventUserService.ts
+++ src/modules/event/services/eventUser/UpdateEventUserService.ts
@@ -2,12 +2,13 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '@shared/infra/http/errors/AppError';
 import EventUser from '@modules/event/infra/typeorm/entities/EventUser';
 import EventTypeUser from '@modules/event/infra/typeorm/entities/EventTypeUser';
+import IDatefnsProvider from '@shared/providers/DatefnsProvider/models/IDatefnsProvider';
 import IEventUserRepository from '../../repositories/IEventUserRepository';
 
 interface IRequest {
   eventUser_id: string;
   name: string;
-  date: Date;
+  date: string;
   start_time: Date;
   end_time: Date;
   description: string;
@@ -19,6 +20,9 @@ export default class UpdateEventUserService {
   constructor(
     @inject('EventUserRepository')
     private eventUserRepository: IEventUserRepository,
+
+    @inject('DatefnsProvider')
+    private dateFnsProvider: IDatefnsProvider,
   ) {}
 
   public async execute({
@@ -34,6 +38,12 @@ export default class UpdateEventUserService {
 
     if (!eventUser) {
       throw new AppError('Event Type does not exists.', 404);
+    }
+
+    const thisDateisAfter = await this.dateFnsProvider.thisDateIsAfter(date);
+
+    if (!thisDateisAfter) {
+      throw new AppError('The date provided is not valid', 400);
     }
 
     Object.assign(eventUser, {

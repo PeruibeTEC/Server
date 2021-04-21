5de023d35352cdd8390541107fa81e9e07cd31ac
João Victor da Silva <jv782063@gmail.com>
2021-04-21 17:53:40 -0300

    refactor(ensure-authenticate): changes the authentication middleware to the shared folder

--- src/modules/event/infra/http/routes/eventUser.routes.ts
+++ src/modules/event/infra/http/routes/eventUser.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import ensureAuthenticate from '@shared/infra/http/middlewares/ensureAuthenticate';
 import EventUserController from '../controllers/EventUserController';
 
 const eventUserRouter = Router();
@@ -7,8 +8,16 @@ const eventUserController = new EventUserController();
 
 eventUserRouter.get('/', eventUserController.index);
 eventUserRouter.get('/:event_id', eventUserController.show);
-eventUserRouter.post('/', eventUserController.create);
-eventUserRouter.put('/:event_id', eventUserController.update);
-eventUserRouter.delete('/:event_id', eventUserController.delete);
+eventUserRouter.post('/', ensureAuthenticate, eventUserController.create);
+eventUserRouter.put(
+  '/:event_id',
+  ensureAuthenticate,
+  eventUserController.update,
+);
+eventUserRouter.delete(
+  '/:event_id',
+  ensureAuthenticate,
+  eventUserController.delete,
+);
 
 export default eventUserRouter;

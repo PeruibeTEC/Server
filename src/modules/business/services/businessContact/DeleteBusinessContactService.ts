5cb9642698ff41220cbbbfa6024b25058fb3f38c
PocketLocked <bertoluzzi2004@gmail.com>
2021-04-29 16:27:03 -0300

    fix(business): fix DeleteBusinessContact service

--- src/modules/business/services/businessContact/DeleteBusinessContactService.ts
+++ src/modules/business/services/businessContact/DeleteBusinessContactService.ts
@@ -11,7 +11,7 @@ interface IRequest {
 @injectable()
 export default class DeleteBusinessContactService {
   constructor(
-    @inject('BusinessContacyRepository')
+    @inject('BusinessContactRepository')
     private businessContactRepository: IBusinessContactRepository,
   ) {}
 

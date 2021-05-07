601bb9c655b9942c03ca738b8eddc8aede218284
PocketLocked <bertoluzzi2004@gmail.com>
2021-05-07 00:27:01 -0300

    fix(utils): created undefined value handling

--- src/shared/utils/averageRatingFunction.ts
+++ src/shared/utils/averageRatingFunction.ts
@@ -1,4 +1,10 @@
-export default function averageRating(ratingValues: Array<number>): string {
+export default function averageRating(
+  ratingValues: Array<number> | undefined,
+): string {
+  if (ratingValues === undefined) {
+    return 'Value is undefined';
+  }
+
   let sumRatingValues = 0;
   for (let i = 0; i < ratingValues.length; i = 1 + i) {
     sumRatingValues += ratingValues[i];

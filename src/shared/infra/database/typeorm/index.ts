1903b17944d5ee7fea488445c7f1a593de08177f
hmartiins <hmartins224@gmail.com>
2021-06-01 15:48:56 -0300

    feat: add credentials for connection in database

--- src/shared/infra/database/typeorm/index.ts
+++ src/shared/infra/database/typeorm/index.ts
@@ -1,3 +1,10 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+createConnection({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});

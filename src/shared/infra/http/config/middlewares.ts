6723e322db563f2a8947ae233a9876120afbdefb
dio99o <diogo.braz01@etec.sp.gov.br>
2021-07-02 19:49:23 -0300

    feat(security): added helmet package

--- src/shared/infra/http/config/middlewares.ts
+++ src/shared/infra/http/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { bodyParser, contentType, cors } from '../middlewares';
+import helmet from 'helmet';
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
   app.use(contentType);
+  app.use(helmet());
 };

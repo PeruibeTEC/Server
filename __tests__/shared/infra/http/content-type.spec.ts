297bb10974867101615d07bc032779308b255d4b
hmartiins <hmartins224@gmail.com>
2021-07-04 21:48:04 -0300

    refactor: refactor config for project and refactor tests

--- __tests__/shared/infra/http/content-type.spec.ts
+++ __tests__/shared/infra/http/content-type.spec.ts
@@ -5,7 +5,6 @@ import request from 'supertest';
 describe('Content Type Middleware', () => {
   test('Should return default content type as json', async () => {
     app.get('/test_content_type', (req, res) => {
-      res.type('json');
       res.send('');
     });
     await request(app).get('/test_content_type').expect('content-type', /json/);

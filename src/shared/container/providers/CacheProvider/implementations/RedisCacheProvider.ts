460f9f04360b6af82d3fbeaaf5fd42c59283577b
dio99o <diogo.braz01@etec.sp.gov.br>
2021-07-13 21:04:31 -0300

    feat(cache): first steps in configuring redis

---
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,18 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '../../../../../config/cache';
+import ICacheProvider from '../models/ICacheProvider';
+
+export default class RedisCacheProvider implements ICacheProvider {
+  private client: Redis;
+
+  constructor() {
+    console.log('asd');
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  save(key: string, value: string): Promise<void> {}
+
+  recover(key: string): Promise<string> {}
+
+  invalidate(key: string): Promise<void> {}
+}
